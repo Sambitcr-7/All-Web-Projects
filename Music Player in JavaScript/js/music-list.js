8b6ceaff65123b5ca9c7db8b8a79374df7398de5
SAMBIT PATRA <84149325+Sambitcr-7@users.noreply.github.com>
2022-07-01 09:21:31 +0530

    Add files via upload

---
+++ Music Player in JavaScript/js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "Hawa Banke",
+    artist: "Darshan",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ambarsariya Fukrey",
+    artist: "Sona Mohapatra",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Akh Lad Jaave",
+    artist: "Loveyatri | Aayush S",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Bekhayali",
+    artist: "ARIJIT SINGH",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "DILBAR",
+    artist: "Neha Kakkar,Dhvani",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Qismat",
+    artist: "B Praak",
+    img: "music-6",
+    src: "music-6"
+  },
+];
